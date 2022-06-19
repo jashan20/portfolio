5f0cbf58e16e0f6d0542b950c7c40ca45cf22440
Jashandeep Singh <jashan21000@gmail.com>
2022-06-19 21:39:03 +0530

    Add routes

--- app.js
+++ app.js
@@ -34,14 +34,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 //Routes
-
-// app.post('/',(req,res)=>{
-//     res.send('index');
-// })
-// app.get('/',(req,res)=>{
-//     console.log("Jashan")
-// })
-
 app.use('/',contactrouter);
 
 //PORT 
